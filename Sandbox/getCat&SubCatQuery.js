ab5640ab05960bbda0149aaa93c7f00e8ca0cba3
Worrapat <gaeworrapat@gmail.com>
2025-11-18 00:03:59 +0700

    SIN Script Include

--- Sandbox/getCat&SubCatQuery.js
+++ Sandbox/getCat&SubCatQuery.js
@@ -1,85 +1,92 @@
-var activities = "34dbf0c8933ededc1b43b27efaba1095";
-var budgetHolderId = "b332de60930e9614c111316efaba1079";
+var activities = '2ab4fd29db73e8507370622fd39619ea'; // Medical Expenses(CTR : C001H02019)
+var budgetHolderId = 'b332de60930e9614c111316efaba1079'; // FFM/O 2025
+
 
 getSINCategoryQuery(activities, budgetHolderId);
 
-function getSINCategoryQuery(activityName, budgetHolderId) {
+function getBudgetHoder(budgetHolderId) {
+    var gr = new GlideRecord('u_istationery_budget_holder');
+    gr.addQuery('sys_id', budgetHolderId);
+    gr.query();a
+    if(gr.next()) {
+        return gr.u_budget_holder;
+    } else {
+        return budgetHolderId;
+    }
+}
 
-	// --- STEP 1: หา SIN Category จาก Activity ---
-	var grAct = new GlideRecord('u_ist_support_activities');
-	grAct.addQuery('u_activities_name', activityName);
-	grAct.query();
+function getSINCategoryQuery(activities, budgetHolderId) {
+    var budgetHolder = getBudgetHoder(budgetHolderId);
+    var sinCatList = [];
 
-	var sinList = [];
-	if (grAct.next()) {
-		if (grAct.u_sin_category) {
-			sinList = (grAct.u_sin_category + "").split(',');
-		}
-	}
+    // --- STEP 1: หา SIN Category จาก Activity ---
+    var grUIA = new GlideRecord('u_ist_support_activities');
+    if(grUIA.get(activities)) {
+        gs.info('u_activities_name: ' + grUIA.getValue('u_activities_name'));
+        gs.info('grUIA.u_sin_category: ' + grUIA.u_sin_category);
+        if(grUIA.u_sin_category) {
+            sinCatList = (grUIA.u_sin_category + '').split(',');
+        }
 
-	if (sinList.length === 0) {
-		gs.info("[SIN] No SIN Category found for: " + activityName);
-		return "sys_id=null";
-	}
+        if(sinCatList.length === 0) {
+            gs.info('[Cat-SIN] No SIN Category found for: ' + activityName);
+            return 'sys_id=null';
+        }
+    }
+    gs.info('sinCatList: ' + sinCatList);
 
-
-	// --- STEP 2: หา Category ที่ budget holder ใช้ได้ ---
-	var grMap = new GlideRecord('u_map_master_item_holder');
-	grMap.addEncodedQuery("u_budget_holder.u_budget_holder=" + budgetHolderId + " ^u_category=" + sinList.join(','));
-	grMap.query();
-
-	var categories = [];
-	while (grMap.next()) {
-		if (grMap.u_category) {
-			categories.push(grMap.u_category.toString());
-		}
-	}
-
-	if (categories.length === 0) {
-		gs.info("[MAP] No mapped categories for budget holder.");
-		return "sys_id=null";
-	}
-
-	console.log("sys_idIN : " + categories.join(','));
-	return "sys_idIN" + categories.join(',');
+    // --- STEP 2: หา Category ที่ budget holder ใช้ได้ ---
+    var grMap = new GlideRecord('u_map_master_item_holder');
+    grMap.addEncodedQuery('u_budget_holder=' + budgetHolder + '^u_category=' + sinCatList.join(','));
+    grMap.query();
+    var categories = [];
+    while(grMap.next()) {
+        if(grMap.u_category) {
+            categories.push(grMap.u_category.toString());
+            gs.info('categories - ' + categories.join(','));
+            return 'sys_idIN' + categories.join(',');
+        } else {
+            gs.info('[MAP] No mapped categories for budget holder.');
+            return 'sys_id=null';
+        }
+    }
 }
 
 getSINSubCategoryQuery(activities, budgetHolderId);
+
 function getSINSubCategoryQuery(activityName, budgetHolderId) {
-	// --- STEP 1: หา SIN Sub Category จาก Activity ---
-	var grAct = new GlideRecord('u_ist_support_activities');
-	grAct.addQuery('u_activities_name', activityName);
-	grAct.query();
+    var budgetHolder = getBudgetHoder(budgetHolderId);
+    var sinSubList = [];
 
-	var sinSubList = [];
-	if (grAct.next()) {
-		if (grAct.u_sin_sub_category) {
-			sinSubList = (grAct.u_sin_sub_category + '').split(',');
-		}
-	}
+    // --- STEP 1: หา SIN Category จาก Activity ---
+    var grUIA = new GlideRecord('u_ist_support_activities');
+    if(grUIA.get(activityName)) {
+        gs.info('u_activities_name: ' + grUIA.getValue('u_activities_name'));
+        if(grUIA.u_sin_sub_category) {
+            sinSubList = (grUIA.u_sin_sub_category + '').split(',');
+        }
 
-	if (sinSubList.length === 0) {
-		gs.info('[Sub-SIN] No SIN Sub Category found for: ' + activityName);
-		return 'sys_id=null';
-	}
+        if(sinSubList.length === 0) {
+            gs.info('[Sub-SIN] No SIN Sub Category found for: ' + activityName);
+            return 'sys_id=null';
+        }
+    }
 
-	// --- STEP 2: หา SubCategory ที่ Budget Holder ใช้ได้ ---
-	var grMap = new GlideRecord('u_map_master_item_holder');
-	console.log("sinSubList.join(',') " + sinSubList.join(','));
-	grMap.addEncodedQuery("u_budget_holder.u_budget_holder=" + budgetHolderId + " ^u_subcategory=" + sinSubList.join(','));
-	grMap.query();
-
-	var mappedSub = [];
-	while (grMap.next()) {
-		if (grMap.u_subcategory) {
-			mappedSub.push(grMap.u_subcategory.toString());
-		}
-	}
-
-	if (mappedSub.length === 0) {
-		gs.info('[MAP] No mapped sub-category for budget holder.');
-		return 'sys_id=null';
-	}
-
-	return 'sys_idIN' + mappedSub.join(',');
+    // --- STEP 2: หา Category ที่ budget holder ใช้ได้ ---
+    var grMap = new GlideRecord('u_map_master_item_holder');
+    grMap.addEncodedQuery(
+        'u_budget_holder=' + budgetHolder + '^u_subcategory=' + sinSubList.join(','),
+    );
+    grMap.query();
+    var subCategories = [];
+    while(grMap.next()) {
+        if(grMap.u_subcategory) {
+            subCategories.push(grMap.u_subcategory.toString());
+            gs.info('subCategories - ' + subCategories.join(','));
+            return 'sys_idIN' + subCategories.join(',');
+        } else {
+            gs.info('[MAP] No mapped categories for budget holder.');
+            return 'sys_id=null';
+        }
+    }
 }
